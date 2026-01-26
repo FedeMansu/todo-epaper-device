286e8daf0d5f0085eaa60e2dbc6a547d2a082712
Federico Mansutti <yeeeegiga40@gmail.com>
2026-01-26 20:48:19 +0100

    feat: todo service

--- frontend/src/app/services/todo.ts
+++ frontend/src/app/services/todo.ts
@@ -1,20 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Todo } from '../dtos/todo.dto';
+import { Todo, TodosResponse } from '../dtos/todo.dto';
+import { environment } from '../../environments/env';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
   private http = inject(HttpClient);
-  private apiUrl = '/api';
 
-  public getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.apiUrl.concat('/todos'));
+  public getTodos(): Observable<TodosResponse> {
+    return this.http.get<TodosResponse>(environment.apiUrl.concat('/todos'));
   }
 
   public createTodo(todo: Partial<Todo>): Observable<Todo> {
-    return this.http.post<Todo>(this.apiUrl.concat('/todos'), todo);
+    return this.http.post<Todo>(environment.apiUrl.concat('/todos'), todo);
+  }
+
+  public deleteTodo(todo: Partial<Todo>): Observable<Todo> {
+    return this.http.post<Todo>(environment.apiUrl.concat('/todos'), todo);
   }
 }
